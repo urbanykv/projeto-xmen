7970941c004d58b8f4a26720d2d08821efa9dc9c
Matheus Urban <matheusurban19@gmail.com>
2023-07-17 19:56:23 -0300

    Projeto X-Men

---
+++ src/js/script.js
@@ -0,0 +1,367 @@
+function botaoc(){
+    if(iciclope.style.display == 'none'){
+        iciclope.style.display = 'flex';
+        main.style.gap = '60px'
+    }else{
+        iciclope.style.display = 'none';
+        main.style.gap = '0px'}
+}
+
+function removec(){
+    if(ijean.style.display == 'flex'){
+        ijean.style.display = 'none'
+    }else{
+        ijean.style.display = 'none'}
+
+    if(ilince.style.display == 'flex'){
+        ilince.style.display = 'none'
+    }else{
+        ilince.style.display = 'none'}
+        
+    if(itempestade.style.display == 'flex'){
+        itempestade.style.display = 'none'
+    }else{
+        itempestade.style.display = 'none'}
+
+    if(ivampira.style.display == 'flex'){
+        ivampira.style.display = 'none'
+    }else{
+        ivampira.style.display = 'none'}
+
+    if(iwolverine.style.display == 'flex'){
+        iwolverine.style.display = 'none'
+    }else{
+        iwolverine.style.display = 'none'}
+
+    if(inoturno.style.display == 'flex'){
+        inoturno.style.display = 'none'
+    }else{
+        inoturno.style.display = 'none'}
+
+    if(imagneto.style.display == 'flex'){
+        imagneto.style.display = 'none'
+    }else{
+        imagneto.style.display = 'none'}
+}
+
+function botaoj(){
+    if(ijean.style.display == 'none'){
+        ijean.style.display = 'flex';
+        main.style.gap = '60px'
+    }else{
+        ijean.style.display = 'none';
+        main.style.gap = '0px'}
+}
+
+function removej(){
+    if(iciclope.style.display == 'flex'){
+        iciclope.style.display = 'none'
+    }else{
+        iciclope.style.display = 'none'}
+
+    if(ilince.style.display == 'flex'){
+        ilince.style.display = 'none'
+    }else{
+        ilince.style.display = 'none'}
+        
+    if(itempestade.style.display == 'flex'){
+        itempestade.style.display = 'none'
+    }else{
+        itempestade.style.display = 'none'}
+
+    if(ivampira.style.display == 'flex'){
+        ivampira.style.display = 'none'
+    }else{
+        ivampira.style.display = 'none'}
+
+    if(iwolverine.style.display == 'flex'){
+        iwolverine.style.display = 'none'
+    }else{
+        iwolverine.style.display = 'none'}
+
+    if(inoturno.style.display == 'flex'){
+        inoturno.style.display = 'none'
+    }else{
+        inoturno.style.display = 'none'}
+
+    if(imagneto.style.display == 'flex'){
+        imagneto.style.display = 'none'
+    }else{
+        imagneto.style.display = 'none'}
+}
+
+function botaol(){
+    if(ilince.style.display == 'none'){
+        ilince.style.display = 'flex';
+        main.style.gap = '60px'
+    }else{
+        ilince.style.display = 'none';
+        main.style.gap = '0px'}
+}
+
+function removel(){
+    if(ijean.style.display == 'flex'){
+        ijean.style.display = 'none'
+    }else{
+        ijean.style.display = 'none'}
+
+    if(iciclope.style.display == 'flex'){
+        iciclope.style.display = 'none'
+    }else{
+        iciclope.style.display = 'none'}
+        
+    if(itempestade.style.display == 'flex'){
+        itempestade.style.display = 'none'
+    }else{
+        itempestade.style.display = 'none'}
+
+    if(ivampira.style.display == 'flex'){
+        ivampira.style.display = 'none'
+    }else{
+        ivampira.style.display = 'none'}
+
+    if(iwolverine.style.display == 'flex'){
+        iwolverine.style.display = 'none'
+    }else{
+        iwolverine.style.display = 'none'}
+
+    if(inoturno.style.display == 'flex'){
+        inoturno.style.display = 'none'
+    }else{
+        inoturno.style.display = 'none'}
+
+    if(imagneto.style.display == 'flex'){
+        imagneto.style.display = 'none'
+    }else{
+        imagneto.style.display = 'none'}
+}
+
+function botaot(){
+    if(itempestade.style.display == 'none'){
+        itempestade.style.display = 'flex';
+        main.style.gap = '60px'
+    }else{
+        itempestade.style.display = 'none';
+        main.style.gap = '0px'}
+}
+
+function removet(){
+    if(ijean.style.display == 'flex'){
+        ijean.style.display = 'none'
+    }else{
+        ijean.style.display = 'none'}
+
+    if(ilince.style.display == 'flex'){
+        ilince.style.display = 'none'
+    }else{
+        ilince.style.display = 'none'}
+        
+    if(iciclope.style.display == 'flex'){
+        iciclope.style.display = 'none'
+    }else{
+        iciclope.style.display = 'none'}
+
+    if(ivampira.style.display == 'flex'){
+        ivampira.style.display = 'none'
+    }else{
+        ivampira.style.display = 'none'}
+
+    if(iwolverine.style.display == 'flex'){
+        iwolverine.style.display = 'none'
+    }else{
+        iwolverine.style.display = 'none'}
+
+    if(inoturno.style.display == 'flex'){
+        inoturno.style.display = 'none'
+    }else{
+        inoturno.style.display = 'none'}
+
+    if(imagneto.style.display == 'flex'){
+        imagneto.style.display = 'none'
+    }else{
+        imagneto.style.display = 'none'}
+}
+
+function botaov(){
+    if(ivampira.style.display == 'none'){
+        ivampira.style.display = 'flex';
+        main.style.gap = '60px'
+    }else{
+        ivampira.style.display = 'none';
+        main.style.gap = '0px'}
+}
+
+function removev(){
+    if(ijean.style.display == 'flex'){
+        ijean.style.display = 'none'
+    }else{
+        ijean.style.display = 'none'}
+
+    if(ilince.style.display == 'flex'){
+        ilince.style.display = 'none'
+    }else{
+        ilince.style.display = 'none'}
+        
+    if(itempestade.style.display == 'flex'){
+        itempestade.style.display = 'none'
+    }else{
+        itempestade.style.display = 'none'}
+
+    if(iciclope.style.display == 'flex'){
+        iciclope.style.display = 'none'
+    }else{
+        iciclope.style.display = 'none'}
+
+    if(iwolverine.style.display == 'flex'){
+        iwolverine.style.display = 'none'
+    }else{
+        iwolverine.style.display = 'none'}
+
+    if(inoturno.style.display == 'flex'){
+        inoturno.style.display = 'none'
+    }else{
+        inoturno.style.display = 'none'}
+
+    if(imagneto.style.display == 'flex'){
+        imagneto.style.display = 'none'
+    }else{
+        imagneto.style.display = 'none'}
+}
+
+function botaow(){
+    if(iwolverine.style.display == 'none'){
+        iwolverine.style.display = 'flex';
+        main.style.gap = '60px'
+    }else{
+        iwolverine.style.display = 'none';
+        main.style.gap = '0px'}
+}
+
+function removew(){
+    if(ijean.style.display == 'flex'){
+        ijean.style.display = 'none'
+    }else{
+        ijean.style.display = 'none'}
+
+    if(ilince.style.display == 'flex'){
+        ilince.style.display = 'none'
+    }else{
+        ilince.style.display = 'none'}
+        
+    if(itempestade.style.display == 'flex'){
+        itempestade.style.display = 'none'
+    }else{
+        itempestade.style.display = 'none'}
+
+    if(ivampira.style.display == 'flex'){
+        ivampira.style.display = 'none'
+    }else{
+        ivampira.style.display = 'none'}
+
+    if(iciclope.style.display == 'flex'){
+        iciclope.style.display = 'none'
+    }else{
+        iciclope.style.display = 'none'}
+
+    if(inoturno.style.display == 'flex'){
+        inoturno.style.display = 'none'
+    }else{
+        inoturno.style.display = 'none'}
+
+    if(imagneto.style.display == 'flex'){
+        imagneto.style.display = 'none'
+    }else{
+        imagneto.style.display = 'none'}
+}
+
+function botaon(){
+    if(inoturno.style.display == 'none'){
+        inoturno.style.display = 'flex';
+        main.style.gap = '60px'
+    }else{
+        inoturno.style.display = 'none';
+        main.style.gap = '0px'}
+}
+
+function removen(){
+    if(ijean.style.display == 'flex'){
+        ijean.style.display = 'none'
+    }else{
+        ijean.style.display = 'none'}
+
+    if(ilince.style.display == 'flex'){
+        ilince.style.display = 'none'
+    }else{
+        ilince.style.display = 'none'}
+        
+    if(itempestade.style.display == 'flex'){
+        itempestade.style.display = 'none'
+    }else{
+        itempestade.style.display = 'none'}
+
+    if(ivampira.style.display == 'flex'){
+        ivampira.style.display = 'none'
+    }else{
+        ivampira.style.display = 'none'}
+
+    if(iwolverine.style.display == 'flex'){
+        iwolverine.style.display = 'none'
+    }else{
+        iwolverine.style.display = 'none'}
+
+    if(iciclope.style.display == 'flex'){
+        iciclope.style.display = 'none'
+    }else{
+        iciclope.style.display = 'none'}
+
+    if(imagneto.style.display == 'flex'){
+        imagneto.style.display = 'none'
+    }else{
+        imagneto.style.display = 'none'}
+}
+
+function botaom(){
+    if(imagneto.style.display == 'none'){
+        imagneto.style.display = 'flex';
+        main.style.gap = '60px'
+    }else{
+        imagneto.style.display = 'none';
+        main.style.gap = '0px'}
+}
+
+function removem(){
+    if(ijean.style.display == 'flex'){
+        ijean.style.display = 'none'
+    }else{
+        ijean.style.display = 'none'}
+
+    if(ilince.style.display == 'flex'){
+        ilince.style.display = 'none'
+    }else{
+        ilince.style.display = 'none'}
+        
+    if(itempestade.style.display == 'flex'){
+        itempestade.style.display = 'none'
+    }else{
+        itempestade.style.display = 'none'}
+
+    if(ivampira.style.display == 'flex'){
+        ivampira.style.display = 'none'
+    }else{
+        ivampira.style.display = 'none'}
+
+    if(iwolverine.style.display == 'flex'){
+        iwolverine.style.display = 'none'
+    }else{
+        iwolverine.style.display = 'none'}
+
+    if(inoturno.style.display == 'flex'){
+        inoturno.style.display = 'none'
+    }else{
+        inoturno.style.display = 'none'}
+
+    if(iciclope.style.display == 'flex'){
+        iciclope.style.display = 'none'
+    }else{
+        iciclope.style.display = 'none'}
+}
